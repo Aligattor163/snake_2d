e26311d06ec795a82858e79d9e9304a8990b4cea
Oleg Shalyakin <Aligattor@mail.ru>
2020-03-01 03:30:47 +0400

    initial commit

--- SnakeGame.ts
+++ SnakeGame.ts
@@ -6,6 +6,14 @@ class SnakeGame {
         let canvas = document.getElementById("snake_viewport") as HTMLCanvasElement;
         let context = canvas.getContext('2d');
 
+        // TBD
+
+        this.canvas = canvas;
+        this.context = context;
+        this.reDraw();
+    }
+
+    private reDraw(){
 
     }
 }
