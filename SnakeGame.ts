1ffa7d2924e8dd01563fb12996be1cc297547e0e
Oleg Shalyakin <Aligattor@mail.ru>
2020-03-02 00:59:01 +0400

    the final part

--- SnakeGame.ts
+++ SnakeGame.ts
@@ -35,7 +35,7 @@ let snake = Array<FieldPosition>();
 snake[0] = {
     x: 9 * box,
     y: 10 * box
-}
+};
 
 // Set listener for arrow control
 document.addEventListener('keydown', setDirection);
@@ -58,47 +58,67 @@ function setDirection(event): void {
     }
 }
 
+function checkCollision(head: FieldPosition, arr: Array<FieldPosition>) {
+    for (let i = 0; i < arr.length; i++) {
+        if (JSON.stringify(arr[i]) == JSON.stringify(head)) {
+            clearInterval(game);
+        }
+    }
+}
+
 // Main function for redraw game in canvas
 function drawGame(): void {
 
     context.drawImage(fieldImg, 0, 0);
     context.drawImage(appleImg, apple.x, apple.y);
 
+    // Draw snake
     for (let i = 0; i < snake.length; i++) {
-
-        context.fillStyle = 'green';
+        context.fillStyle = i == 0 ? 'yellow' : 'green';
         context.fillRect(snake[i].x, snake[i].y, box, box);
-        context.fillStyle = 'white';
-
-        context.font = '50px Arial';
-        context.fillText(score.toString(), box * 2, box * 1.7);
-
-        let snakeLastPosition: FieldPosition = {
-            x: snake[0].x,
-            y: snake[0].y
-        }
-
-        // Check if snake eats apple
-        if (snakeLastPosition.x == apple.x && snakeLastPosition.y == apple.y) {
-            apple = getNewPosition();
-        } else {
-            // delete the last snake segment
-            snake.pop();
-        }
-
-        if (direction == 'left') snakeLastPosition.x -= box;
-        if (direction == 'right') snakeLastPosition.x += box;
-        if (direction == 'up') snakeLastPosition.y -= box;
-        if (direction == 'down') snakeLastPosition.y += box;
-
-        let snakeNewHead: FieldPosition = {
-            x: snakeLastPosition.x,
-            y: snakeLastPosition.y
-        }
-
-        // add (move) new snake segment to the field
-        snake.unshift(snakeNewHead);
     }
+
+    // Draw score
+    context.fillStyle = 'white';
+    context.font = '50px Arial';
+    context.fillText(score.toString(), box * 2, box * 1.7);
+
+    let snakeLastPosition: FieldPosition = {
+        x: snake[0].x,
+        y: snake[0].y
+    };
+
+    // Check if snake eats apple
+    if (snakeLastPosition.x == apple.x && snakeLastPosition.y == apple.y) {
+        score++;
+        apple = getNewPosition();
+    } else {
+        // delete the last snake segment
+        snake.pop();
+    }
+
+    // Check if snake overlaps the borders
+    if (snakeLastPosition.x == 0 || snakeLastPosition.x == box * 18 || snakeLastPosition.y == box * 2 || snakeLastPosition.y == box * 18) {
+        context.fillStyle = 'red';
+        context.fillRect(snakeLastPosition.x, snakeLastPosition.y, box, box);
+        clearInterval(game);
+    }
+
+
+    if (direction == 'left') snakeLastPosition.x -= box;
+    if (direction == 'right') snakeLastPosition.x += box;
+    if (direction == 'up') snakeLastPosition.y -= box;
+    if (direction == 'down') snakeLastPosition.y += box;
+
+    let snakeNewHead: FieldPosition = {
+        x: snakeLastPosition.x,
+        y: snakeLastPosition.y
+    };
+
+    checkCollision(snakeLastPosition, snake);
+
+    // add (move) new snake segment to the field
+    snake.unshift(snakeNewHead);
 }
 
 // Make canvas updates iterative
