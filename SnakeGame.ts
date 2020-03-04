1a1c74da16ae56a831707e8ed046c738d447e9a3
Oleg Shaliakin <oleg.shaliakin@NetCracker.com>
2020-03-04 13:13:05 +0400

    redesign the game

--- SnakeGame.ts
+++ SnakeGame.ts
@@ -11,14 +11,14 @@ const context: CanvasRenderingContext2D = canvas.getContext('2d');
 // Set images
 let fieldImg: HTMLImageElement = new Image();
 let appleImg: HTMLImageElement = new Image();
-let snakeHeadImg: HTMLImageElement = new Image(32, 32);
-let snakeDiedImg: HTMLImageElement = new Image(32, 32);
-let snakeBodyImg: HTMLImageElement = new Image(32, 32);
+let snakeHeadImg: HTMLImageElement = new Image();
+let snakeDiedImg: HTMLImageElement = new Image();
+let snakeBodyImg: HTMLImageElement = new Image();
 
 fieldImg.src = 'img/field.png';
 appleImg.src = 'img/apple.png';
 snakeHeadImg.src = 'img/snake_head.png';
-snakeDiedImg.src = 'img/snake_died_head.png';
+snakeDiedImg.src = 'img/snake_died.png';
 snakeBodyImg.src = 'img/snake_body.png';
 
 const box: number = 32;
@@ -85,6 +85,7 @@ function setDirection(event): void {
 function checkCollision(head: FieldPosition, arr: Array<FieldPosition>) {
     for (let i = 0; i < arr.length; i++) {
         if (JSON.stringify(arr[i]) == JSON.stringify(head)) {
+            drawHead(snakeDiedImg, arr[0].x, arr[0].y);
             clearInterval(game);
         }
     }
@@ -124,9 +125,7 @@ function drawGame(): void {
     // Draw snake
     for (let i = 0; i < snake.length; i++) {
         if (i > 0) {
-            // context.fillStyle = 'blue';
-            // context.fillRect(snake[i].x, snake[i].y, box, box);
-            context.drawImage(snakeBodyImg, snake[i].x, snake[i].y);
+            context.drawImage(snakeBodyImg, snake[i].x + 2, snake[i].y + 2, 28, 28);
         }
         drawHead(snakeHeadImg, snake[0].x, snake[0].y);
     }
@@ -134,19 +133,19 @@ function drawGame(): void {
     // Draw score
     context.fillStyle = 'white';
     context.font = '50px Arial';
-    context.fillText(score.toString(), box * 2, box * 1.7);
+    context.fillText(score.toString(), box * 2, box * 1.6);
 
     context.fillStyle = 'white';
     context.font = '50px Arial';
-    context.fillText(speed.toString(), box * 7, box * 1.7);
+    context.fillText(speed.toString(), box * 7, box * 1.6);
 
-    let snakeLastPosition: FieldPosition = {
+    let snakeHeadLastPosition: FieldPosition = {
         x: snake[0].x,
         y: snake[0].y
     };
 
     // Check if snake eats apple
-    if (snakeLastPosition.x == apple.x && snakeLastPosition.y == apple.y) {
+    if (snakeHeadLastPosition.x == apple.x && snakeHeadLastPosition.y == apple.y) {
         score++;
         apple = getNewPosition();
         intervalValue -= 5;
@@ -159,22 +158,23 @@ function drawGame(): void {
     }
 
     // Check if snake overlaps the borders
-    if (snakeLastPosition.x == 0 || snakeLastPosition.x == box * 18 || snakeLastPosition.y == box * 2 || snakeLastPosition.y == box * 18) {
-        drawHead(snakeDiedImg, snakeLastPosition.x, snakeLastPosition.y)
+    if (snakeHeadLastPosition.x == 0 || snakeHeadLastPosition.x == box * 18 || snakeHeadLastPosition.y == box * 2 || snakeHeadLastPosition.y == box * 18) {
+
+        drawHead(snakeDiedImg, snakeHeadLastPosition.x, snakeHeadLastPosition.y);
         clearInterval(game);
     }
 
-    if (direction == 'left') snakeLastPosition.x -= box;
-    if (direction == 'right') snakeLastPosition.x += box;
-    if (direction == 'up') snakeLastPosition.y -= box;
-    if (direction == 'down') snakeLastPosition.y += box;
+    if (direction == 'left') snakeHeadLastPosition.x -= box;
+    if (direction == 'right') snakeHeadLastPosition.x += box;
+    if (direction == 'up') snakeHeadLastPosition.y -= box;
+    if (direction == 'down') snakeHeadLastPosition.y += box;
 
     let snakeNewHead: FieldPosition = {
-        x: snakeLastPosition.x,
-        y: snakeLastPosition.y
+        x: snakeHeadLastPosition.x,
+        y: snakeHeadLastPosition.y
     };
 
-    checkCollision(snakeLastPosition, snake);
+    checkCollision(snakeHeadLastPosition, snake);
 
     // add (move) new snake segment to the field
     snake.unshift(snakeNewHead);
